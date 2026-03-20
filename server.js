c1d6ac26c2efc857c17d32e7f865536cdeeda004
Shashikumar Mulimani <shree.mata.hbl@gmail.com>
2026-03-20 16:40:13 +0530

    first commit1

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.static('public'));
 
 
 // ================== MONGODB ==================
-mongoose.connect('mongodb://localhost:27017/chat_app')
+mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log("MongoDB Connected"))
     .catch(err => console.log(err));
 
